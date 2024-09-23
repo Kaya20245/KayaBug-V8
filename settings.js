ad565367416f2a5503040789082b04fc29217a13
Kaya20245 <kayakaneki@gmail.com>
2024-09-23 18:03:26 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,26 +10,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "243823661981"
+global.ownername = "KAYA"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '243823661981'  //creator number
+global.ownername = 'KAYA' //owner name
+global.botname = 'XeonBug-V9 VIP' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.packname = 'KAYA'
+global.author = 'GlobalTech\n\nContact: +243823661981'
 
 //console view/theme
 global.themeemoji = '🪀'
 global.wm = "Global Bot"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.link = 'https://whatsapp.com/channel/0029VaiPkRPLY6d0qEX50e2k'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
